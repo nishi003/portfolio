d425855fbdcd0fcd7266ddfc75c6f16b93948b27
Nishi <humayuranishi@gmail.com>
2024-03-07 23:34:01 -0500

    navbar somewhat done

--- personal-website/src/components/NavBar/index.jsx
+++ personal-website/src/components/NavBar/index.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Container, Navbar, Nav, NavDropdown, Button, Fade } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import logo from '../../assets/img/logo.svg'
+import navIcon1 from '../../assets/img/nav-icon1.svg'
+import navIcon2 from '../../assets/img/nav-icon2.svg'
+import navIcon3 from '../../assets/img/nav-icon3.svg'
 
 const Index = () => {
     const [activeLink, setActiveLink] = useState("about");
@@ -29,7 +33,7 @@ const Index = () => {
             <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img src={''} alt="logo" />
+                        <img src={logo} alt="logo" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className="navbar-toggler-icon"></span>
@@ -55,9 +59,9 @@ const Index = () => {
                         <span className="navbar-text">
                             <div className="social-icon">
                                 <Link to="#">
-                                    <img src={''} alt="" />
-                                    <img src={''} alt="" />
-                                    <img src={''} alt="" />
+                                    <img src={navIcon1} alt="" />
+                                    <img src={navIcon2} alt="" />
+                                    <img src={navIcon2} alt="" />
                                 </Link>
                             </div>
                         </span>
